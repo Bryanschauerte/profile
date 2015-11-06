ba3f21b1056897bdc049f579fc987d5a6235ad3c
Bryan Schauerte <bryan.schauerte@gmail.com>
2015-11-05 23:37:58 -0700

    sizing prob corrected

--- public/app/features/pictureDirective/pictureDirectiveCtrl.js
+++ public/app/features/pictureDirective/pictureDirectiveCtrl.js
@@ -10,7 +10,7 @@ angular.module('profileApp').controller('pictureDirectiveCtrl', function($scope)
 
 
     $scope.rotatePictures = function(){
-      $scope.picturesArray = ["./app/images/IMG_1072.jpg", "./app/images/SmallK.png", "./app/images/IMG_1067.jpg"];
+      $scope.picturesArray = ["./app/images/IMG_1072.jpg", "./app/images/SmallK.png", "./app/images/IMG_1067.jpg","./app/images/Pianome.JPG"];
 
       if($scope.pictureIndex >= $scope.picturesArray.length){
         $scope.pictureIndex = 0;
